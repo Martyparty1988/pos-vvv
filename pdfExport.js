bc747ab3c88a9e48b3135fec919c09a0230b7eba
Marty <rosary-vassals8m@icloud.com>
2025-03-11 11:42:46 +0100

    Update pdfExport.js

--- pdfExport.js
+++ pdfExport.js
@@ -1,56 +1,31 @@
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
-
 class PDFExporter {
-    constructor() {
-        this.doc = new jsPDF();
-    }
-
-    // Vytvoření PDF z dat skladu
-    exportInventory(inventory, options = {}) {
-        const {
-            title = 'Stav skladu',
-            fileName = 'sklad.pdf'
-        } = options;
-
-        // Nastavení češtiny
-        this.doc.setLanguage('cs');
-        
-        // Přidání titulku
-        this.doc.setFontSize(20);
-        this.doc.text(title, 14, 20);
-        
-        // Příprava dat pro tabulku
-        const headers = [['ID', 'Název', 'Cena', 'Množství']];
-        const data = inventory.map(item => [
-            item.id,
-            item.name,
-            `${item.price} Kč`,
-            item.stock
-        ]);
-
-        // Vytvoření tabulky
-        this.doc.autoTable({
-            head: headers,
-            body: data,
-            startY: 30,
-            styles: {
-                fontSize: 10,
-                cellPadding: 5
-            },
-            headStyles: {
-                fillColor: [66, 139, 202]
-            }
-        });
-
-        // Přidání data exportu
-        const date = new Date().toLocaleString('cs-CZ');
-        this.doc.setFontSize(10);
-        this.doc.text(`Exportováno: ${date}`, 14, this.doc.lastAutoTable.finalY + 10);
-
-        // Stažení PDF
-        this.doc.save(fileName);
-    }
+  constructor() {
+    this.doc = new jsPDF();
+  }
+  exportInventory(inventory, options = {}) {
+    const { title = 'Stav skladu', fileName = 'sklad.pdf' } = options;
+    this.doc.setLanguage('cs');
+    this.doc.setFontSize(20);
+    this.doc.text(title, 14, 20);
+    const headers = [['Název', 'Cena', 'Kategorie']];
+    const data = inventory.map(item => [
+      item.name,
+      `${item.price} ${item.currency}`,
+      item.category
+    ]);
+    this.doc.autoTable({
+      head: headers,
+      body: data,
+      startY: 30,
+      styles: { fontSize: 10, cellPadding: 5 },
+      headStyles: { fillColor: [66, 139, 202] }
+    });
+    const date = new Date().toLocaleString('cs-CZ');
+    this.doc.setFontSize(10);
+    this.doc.text(`Exportováno: ${date}`, 14, this.doc.lastAutoTable.finalY + 10);
+    this.doc.save(fileName);
+  }
 }
-
 export { PDFExporter };
